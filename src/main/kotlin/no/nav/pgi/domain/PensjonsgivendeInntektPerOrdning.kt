83a467e9df45cb727d738d763abdedfe70514b0e
Jan Kroken <jan.kroken@nav.no>
2024-08-22 14:31:59 +0200

    gjorde noen felter nullable igjen

--- src/main/kotlin/no/nav/pgi/domain/PensjonsgivendeInntektPerOrdning.kt
+++ src/main/kotlin/no/nav/pgi/domain/PensjonsgivendeInntektPerOrdning.kt
@@ -3,8 +3,8 @@ package no.nav.pgi.domain
 data class PensjonsgivendeInntektPerOrdning(
     val skatteordning: Skatteordning,
     val datoForFastsetting: String,
-    val pensjonsgivendeInntektAvLoennsinntekt: Long,
-    val pensjonsgivendeInntektAvLoennsinntektBarePensjonsdel: Long,
-    val pensjonsgivendeInntektAvNaeringsinntekt: Long,
-    val pensjonsgivendeInntektAvNaeringsinntektFraFiskeFangstEllerFamiliebarnehage: Long,
+    val pensjonsgivendeInntektAvLoennsinntekt: Long?,
+    val pensjonsgivendeInntektAvLoennsinntektBarePensjonsdel: Long?,
+    val pensjonsgivendeInntektAvNaeringsinntekt: Long?,
+    val pensjonsgivendeInntektAvNaeringsinntektFraFiskeFangstEllerFamiliebarnehage: Long?,
 )
